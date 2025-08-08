199d4b198e1e357390bb8b7480c00a3b6b165a00
Sergei Dubinin <hartess@yandex.com>
2025-08-08 20:41:52 +0300

    Adding for verification\Corrections

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -7,26 +7,31 @@ import org.openqa.selenium.WebElement;
 public class MainPage {
     private final WebDriver driver;
 
+    private static final String URL = "https://qa-scooter.praktikum-services.ru/";
+    private final By cookieBannerLocator = By.id("rcc-confirm-button");
+    private final By topOrderButtonLocator = By.cssSelector(".Button_Button__ra12g");
+    private final By bottomOrderButtonLocator = By.cssSelector(".Button_Button__ra12g.Button_Middle__1CSJM");
+
     public MainPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void openMainPage() {
-        driver.get("https://qa-scooter.praktikum-services.ru/");
+        driver.get(URL);
     }
 
     public void closeCookieBanner() {
-        WebElement banner = driver.findElement(By.id("rcc-confirm-button"));
+        WebElement banner = driver.findElement(cookieBannerLocator);
         banner.click();
     }
 
     public void clickTopOrderButton() {
-        WebElement button = driver.findElement(By.cssSelector(".Button_Button__ra12g"));
+        WebElement button = driver.findElement(topOrderButtonLocator);
         button.click();
     }
 
     public void clickBottomOrderButton() {
-        WebElement button = driver.findElement(By.cssSelector(".Button_Button__ra12g.Button_Middle__1CSJM"));
+        WebElement button = driver.findElement(bottomOrderButtonLocator);
         button.click();
     }
 }
--- src/main/java/pages/OrderFormPage.java
+++ src/main/java/pages/OrderFormPage.java
@@ -1,12 +1,30 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 
 public class OrderFormPage {
     private final WebDriver driver;
+
+    private final By firstNameFieldLocator = By.cssSelector("input[placeholder='* Имя']");
+    private final By lastNameFieldLocator = By.cssSelector("input[placeholder='* Фамилия']");
+    private final By addressFieldLocator = By.cssSelector("input[placeholder='* Адрес: куда привезти заказ']");
+    private final By metroDropdownLocator = By.cssSelector(".select-search__input");
+    private final String metroStationLocatorTemplate = ".//*[normalize-space(text())='%s']";
+    private final By phoneFieldLocator = By.cssSelector("input[placeholder='* Телефон: на него позвонит курьер']");
+    private final By nextStepButtonLocator = By.cssSelector(".Button_Button__ra12g.Button_Middle__1CSJM");
+    private final By deliveryDateInputLocator = By.cssSelector(".react-datepicker__input-container input");
+    private final By rentDurationDropdownLocator = By.cssSelector(".Dropdown-arrow");
+    private final By rentDurationOptionLocator = By.xpath("//div[text()='сутки']");
+    private final By blackColorOptionLocator = By.id("black");
+    private final By greyColorOptionLocator = By.id("grey");
+    private final By courierCommentFieldLocator = By.cssSelector("input[placeholder='Комментарий для курьера']");
+    private final By orderConfirmButtonLocator = By.cssSelector(".Button_Button__ra12g.Button_Middle__1CSJM:nth-of-type(2)");
+    private final By confirmationDialogButtonLocator = By.xpath("//button[text()='Да']");
+    private final By successModalHeaderLocator = By.cssSelector(".Order_ModalHeader__3FDaJ .Order_Text__2broi");
+    private final String questionHeadingTemplate = "accordion__heading-%d";
+    private final String answerPanelTemplate = "accordion__panel-%d";
 
     public OrderFormPage(WebDriver driver) {
         this.driver = driver;
@@ -19,93 +37,95 @@ public class OrderFormPage {
         }
 
         public String openAndGetAnswer(int index) {
-            WebElement heading = driver.findElement(By.id("accordion__heading-" + index));
+            By questionHeading = By.id(String.format(questionHeadingTemplate, index));
+            By answerPanel = By.id(String.format(answerPanelTemplate, index));
+
+            WebElement heading = driver.findElement(questionHeading);
             heading.click();
-            WebElement panel = driver.findElement(By.id("accordion__panel-" + index));
+
+            WebElement panel = driver.findElement(answerPanel);
             return panel.getText().trim();
         }
     }
 
-
     public void fillFirstName(String firstName) {
-        WebElement field = driver.findElement(By.cssSelector("input[placeholder='* Имя']"));
+        WebElement field = driver.findElement(firstNameFieldLocator);
         field.sendKeys(firstName);
     }
 
     public void fillLastName(String lastName) {
-        WebElement field = driver.findElement(By.cssSelector("input[placeholder='* Фамилия']"));
+        WebElement field = driver.findElement(lastNameFieldLocator);
         field.sendKeys(lastName);
     }
 
     public void fillAddress(String address) {
-        WebElement field = driver.findElement(By.cssSelector("input[placeholder='* Адрес: куда привезти заказ']"));
+        WebElement field = driver.findElement(addressFieldLocator);
         field.sendKeys(address);
     }
 
+
     public void selectMetroStation(String station) {
-        WebElement dropdown = driver.findElement(By.cssSelector(".select-search__input"));
+        WebElement dropdown = driver.findElement(metroDropdownLocator);
         dropdown.click();
-        WebElement option = driver.findElement(By.xpath(".//*[.='" + station + "']"));
+
+        WebElement option = driver.findElement(By.xpath(String.format(metroStationLocatorTemplate, station)));
         option.click();
     }
 
     public void fillPhone(String phone) {
-        WebElement field = driver.findElement(By.cssSelector("input[placeholder='* Телефон: на него позвонит курьер']"));
+        WebElement field = driver.findElement(phoneFieldLocator);
         field.sendKeys(phone);
     }
 
     public void proceedToSecondStep() {
-        WebElement button = driver.findElement(By.cssSelector(".Button_Button__ra12g.Button_Middle__1CSJM"));
+        WebElement button = driver.findElement(nextStepButtonLocator);
         button.click();
     }
 
     public void pickDeliveryDate(String date) {
-        WebElement input = driver.findElement(By.cssSelector(".react-datepicker__input-container input"));
+        WebElement input = driver.findElement(deliveryDateInputLocator);
         input.clear();
         input.sendKeys(date);
     }
 
     public void selectRentDuration() {
-        WebElement dropdown = driver.findElement(By.cssSelector(".Dropdown-arrow"));
+        WebElement dropdown = driver.findElement(rentDurationDropdownLocator);
         dropdown.click();
-        WebElement option = driver.findElement(By.xpath("//div[text()='сутки']"));
+        WebElement option = driver.findElement(rentDurationOptionLocator);
         option.click();
     }
 
     public void selectBlackColor() {
-        WebElement colorOption = driver.findElement(By.id("black"));
+        WebElement colorOption = driver.findElement(blackColorOptionLocator);
         colorOption.click();
     }
 
     public void selectGreyColor() {
-        WebElement colorOption = driver.findElement(By.id("grey"));
+        WebElement colorOption = driver.findElement(greyColorOptionLocator);
         colorOption.click();
     }
 
     public void addCourierComment(String comment) {
-        WebElement field = driver.findElement(By.cssSelector("input[placeholder='Комментарий для курьера']"));
+        WebElement field = driver.findElement(courierCommentFieldLocator);
         field.sendKeys(comment);
     }
 
     public void confirmOrder() {
-        WebElement button = driver.findElement(By.cssSelector(".Button_Button__ra12g.Button_Middle__1CSJM:nth-of-type(2)"));
+        WebElement button = driver.findElement(orderConfirmButtonLocator);
         button.click();
     }
 
     public void confirmOrderDialog() {
-        WebElement dialogButton = driver.findElement(By.xpath("//button[text()='Да']"));
+        WebElement dialogButton = driver.findElement(confirmationDialogButtonLocator);
         dialogButton.click();
     }
 
     public String verifyOrderSuccessModal() {
-        WebElement modalHeader = driver.findElement(By.cssSelector(".Order_ModalHeader__3FDaJ"));
-        return modalHeader.findElement(By.cssSelector(".Order_Text__2broi")).getText();
+        WebElement modalHeader = driver.findElement(successModalHeaderLocator);
+        return modalHeader.getText();
     }
-
-
-
-
 }
+
 
 
 
